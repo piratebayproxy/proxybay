82088eb649e0721e01357df04007d5c7a8d070a3
Unblocked <info@unblocked.co>
2018-02-27 23:59:13 +0100

    Updated

---
+++ README.md
@@ -0,0 +1,6 @@
+# The Proxy Bay
+
+View site on Github: https://proxybay.github.io/
+
+View original site: https://proxybay.bz/
+
---
+++ assets/js/functions.js
@@ -0,0 +1,93 @@
+var $ = function(id){return document.getElementById(id)}
+window.onload = function(){
+  var a = document.getElementsByClassName('speed');
+  var h = document.getElementsByClassName('site');
+  for(var i=0;i<a.length;i++){
+  if(h[i].childNodes[0].href.match(/^https/)){h[i].childNodes[0].innerHTML=h[i].childNodes[0].innerHTML+' <img src="assets/img/secure.gif" width="16" height="16">'}
+  var speed=parseFloat(a[i].innerHTML);
+  if(speed<=1.500){a[i].innerHTML='<span title="Loaded in '+a[i].innerHTML+' seconds">Very Fast</span>';}
+  else if(speed>1.500 && speed<=2.500){a[i].innerHTML='<span title="Loaded in '+a[i].innerHTML+' seconds">Fast</span>';}
+  else if(speed>2.500 && speed<=5.000){a[i].innerHTML='<span title="Loaded in '+a[i].innerHTML+' seconds">Average</span>';}
+  else if(speed>5.000){a[i].innerHTML='<span title="Loaded in '+a[i].innerHTML+' seconds">Slow</span>';}}
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+      if (xmlhttp.readyState == XMLHttpRequest.DONE) {
+         if (!xmlhttp.responseText.match(/good/i)) {
+           var statusobj = document.getElementsByClassName("status");
+           $('dateText').innerHTML = xmlhttp.responseText;
+           for(var i=0;i<statusobj.length;i++){statusobj[i].innerHTML = '<img alt="down" src="assets/img/down.png">';}
+         }
+      }
+  };
+  xmlhttp.open("GET", "status.txt", true);
+  xmlhttp.send();
+}
+
+function reportLink(url){
+  $('reportedSite').value=url;
+  $('reportTitle').innerHTML='Report '+url;
+  report_form_show();
+}
+function report_form_show() {
+  $('reportResponse').innerHTML ='';
+  $('siteBlocked').checked = true;
+  $('reportContainer').style.display = "block";
+}
+function report_form_hide() {
+  $('reportContainer').style.display = "none";
+  $('reportResponse').innerHTML ='';
+  $('siteBlocked').checked = true;
+}
+
+function feedback_form_show() {
+  $('feedbackContainer').style.display = "block";
+  $('feedbackBtn').style.display = "none";
+  $('feedback_email').focus();
+}
+function feedback_form_hide() {
+  $('feedbackContainer').style.display = "none";
+  $('feedbackBtn').style.display = "block";
+  $('feedbackResponse').innerHTML = '';
+}
+
+function submit_feedback(){
+  var feedbackResponse = $('feedbackResponse');
+  if($('feedback_email')){var email=encodeURIComponent($('feedback_email').value);}
+  if($('feedback_message').value.length < 4){feedbackResponse.innerHTML = "Error: Message too short"}
+  else{var message = encodeURIComponent($('feedback_message').value);}
+  var captchaResponse = encodeURIComponent($('g-recaptcha-response').value);
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4) {
+      if (xmlhttp.status==200) {
+        feedbackResponse.innerHTML = xmlhttp.responseText;
+        $('feedback_message').value = '';
+        $('feedback_email').value = '';
+        grecaptcha.reset();
+      }
+    }
+  };
+  xmlhttp.open("POST", "https://s.proxybay.bz/pb_feedback_mail_beta.php", true);
+  xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
+  xmlhttp.send('email='+email+'&message='+message+'&captcharesponse='+captchaResponse);
+  
+}
+
+function submit_report(){
+  var radios = document.getElementsByName('reportSite');
+  for (var i = 0, length = radios.length; i < length; i++){
+   if (radios[i].checked){var feedbackVal = radios[i].value;break;}
+  }
+  var reportedUrl = $('reportedSite').value;
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.onreadystatechange = function() {
+    if (xmlhttp.readyState == 4) {
+      if (xmlhttp.status==200) {
+        $('reportResponse').innerHTML = xmlhttp.responseText;
+      }
+    }
+  };
+  xmlhttp.open("GET", "https://s.proxybay.bz/report.php?reportedUrl="+reportedUrl+'&feedbackVal='+feedbackVal, true);
+  xmlhttp.send();
+}
+
