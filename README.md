8a718d4f0ff7e4397e37c71a17b63154c38f939b
Unblockit <info@unblockit.me>
2023-03-07 12:13:25 +0100

    Updated

--- README.md
+++ README.md
@@ -2,5 +2,5 @@
 
 View mirror site: https://proxybay.pages.dev/
 
-View original site: https://proxy-bay.cam/
+View original site: https://proxy-bay.biz/
 
--- assets/js/functions.js
+++ assets/js/functions.js
@@ -68,7 +68,7 @@ function submit_feedback(){
       }
     }
   };
-  xmlhttp.open("POST", "https://s.proxy-bay.cam/pb_feedback_mail.php", true);
+  xmlhttp.open("POST", "https://s.proxy-bay.biz/pb_feedback_mail.php", true);
   xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
   xmlhttp.send('email='+email+'&message='+message+'&captcharesponse='+captchaResponse);
 }
@@ -88,7 +88,7 @@ function submit_report(){
       }
     }
   };
-  xmlhttp.open("GET", "https://s.proxy-bay.cam/pb_report.php?reportedUrl="+reportedUrl+'&feedbackVal='+feedbackVal, true);
+  xmlhttp.open("GET", "https://s.proxy-bay.biz/pb_report.php?reportedUrl="+reportedUrl+'&feedbackVal='+feedbackVal, true);
   xmlhttp.send();
 }
 
